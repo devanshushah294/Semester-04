d2d4c96b3f9ca3d31cb6ad06e666915f95ee6f26
Devanshu Shah <93487920+devanshushah294@users.noreply.github.com>
2022-11-28 20:09:18 +0530

    Create 01_HelloWorld.py

---
+++ Python Programming/01_HelloWorld.py
@@ -0,0 +1,25 @@
+# This is a comment
+
+# How to print in python
+print("Hello world");
+print("""helloworld""");
+
+# Defining variables in python
+a = 3;
+b = 6;
+
+# Operators in python
+print(a+b);
+print(a-b);
+print(a*b);
+print(a/b);
+print(a//b);
+
+# outputs 
+# Hello world
+# helloworld
+# 9
+# -3
+# 18
+# 0.5
+# 0
