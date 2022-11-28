b08456385310e6c1ce97531e1c24e3f5ecab1a11
Devanshu Shah <93487920+devanshushah294@users.noreply.github.com>
2022-11-28 20:48:10 +0530

    Create 03_DataTypesInPython.py

---
+++ Python Programming/Lab_01/03_DataTypesInPython.py
@@ -0,0 +1,34 @@
+# some basic datatypes in python
+a = 20;
+print(str(type(a)));
+
+b = 30.00;
+print(type(b));
+
+c = True;
+print(type(c));
+
+d = {2,3,4};
+print(type(d));
+
+e = (3,4,5);
+print(type(e));
+
+f = [4,5,6];
+print(type(f));
+
+g = 'g';
+print(type(g));
+
+sstr = "str";
+print(type(sstr));
+
+#outputs
+#<class 'int'>
+# <class 'float'>
+# <class 'bool'>
+# <class 'set'>
+# <class 'tuple'>
+# <class 'list'>
+# <class 'str'>
+# <class 'str'>
