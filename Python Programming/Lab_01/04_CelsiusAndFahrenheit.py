2c7e5b8a90bd794a4fde57d8afe2946ac58eb4b9
Devanshu Shah <93487920+devanshushah294@users.noreply.github.com>
2022-12-05 21:16:37 +0530

    .

--- Python Programming/Lab_01/04_CelsiusAndFahrenheit.py
+++
@@ -1,4 +0,0 @@
-tempInCelsius = int(input("Enter the temp in Celsius"));
-tempInFarhenheit = (9/5)*(tempInCelsius)+32;
-tempInFarhenheit =  str(tempInFarhenheit);
-print("Temp in fahrenheit is: "+ tempInFarhenheit);
