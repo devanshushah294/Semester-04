9ef63dc24688c6fce704dc214ccca5832378f349
Devanshu Shah <93487920+devanshushah294@users.noreply.github.com>
2022-12-10 19:52:05 +0530

    Create 06_PositiveNegative.dart

---
+++ Dart Programming/Lab_03/06_PositiveNegative.dart
@@ -0,0 +1,21 @@
+// Write a dart code program to calculate the sum of all positive even numbers and the sum of all
+// negative odd numbers from a set of numbers. You can enter 0 (zero) to quit the program and
+// thus it displays the result.
+
+import 'dart:io';
+
+void main(List<String> args) {
+  int input = int.parse(stdin.readLineSync()!);
+  int sumOfOddNumbers = 0;
+  int sumOfEvenNumbers = 0;
+  while (input != 0) {
+    if (input > 0 && input % 2 == 0) {
+      sumOfEvenNumbers + sumOfEvenNumbers + input;
+    }
+    if (input < 0 && input % 2 == 1) {
+      sumOfOddNumbers + sumOfOddNumbers + input;
+    }
+    print("Enter the numbers");
+    input = int.parse(stdin.readLineSync()!);
+  }
+}
