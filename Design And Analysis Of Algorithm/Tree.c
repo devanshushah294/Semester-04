6db436a34efe3980cd05d53af7afe02ea688540f
Devanshu Shah <93487920+devanshushah294@users.noreply.github.com>
2022-12-14 20:20:48 +0530

    Create Tree.c

---
+++ Design And Analysis Of Algorithm/Tree.c
@@ -0,0 +1,23 @@
+struct node {
+	int data;
+	struct node *link;
+};
+void printList(struct node *temp){
+	while(temp != '\0'){
+		printf("data%d = %d \n",temp->data,temp->data);
+		temp = temp->link;
+	}
+}
+void main(){
+	struct node *first,*second,*third;
+	first = (struct node *)malloc(sizeof(struct node));
+	second = (struct node *)malloc(sizeof(struct node));
+	third = (struct node *)malloc(sizeof(struct node));
+	first->data = 1;
+	first->link = second;
+	second->data = 2;
+	second->link = third;
+	third->data = 3;
+	third->link = '\0';
+	printList(first);
+}
