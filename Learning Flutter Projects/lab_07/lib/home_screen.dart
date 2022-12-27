68660d404faa145cd6e644739c6c9c26a147e6b5
Devanshu Shah <93487920+devanshushah294@users.noreply.github.com>
2022-12-27 20:15:46 +0530

    Update home_screen.dart

--- Learning Flutter Projects/lab_07/lib/home_screen.dart
+++ Learning Flutter Projects/lab_07/lib/home_screen.dart
@@ -7,7 +7,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(appBar: AppBar(title:Text("Lab - 07")),
       body: Container(
-        child: Row(children: [Container(color: Colors.orange,width: 100,height: 20),Container(color: Colors.white,width: 100,height: 20),Container(color: Colors.green,width: 100,height: 20)],),
+        child:Text("hello from homepage"),
       ),
     );
   }
