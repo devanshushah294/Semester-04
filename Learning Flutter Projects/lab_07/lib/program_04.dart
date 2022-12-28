1ee037bed1e459a6d8e506eebb59bcbdfcb01d20
Devanshu Shah <93487920+devanshushah294@users.noreply.github.com>
2022-12-29 01:52:01 +0530

    Update program_04.dart

--- Learning Flutter Projects/lab_07/lib/program_04.dart
+++ Learning Flutter Projects/lab_07/lib/program_04.dart
@@ -4,73 +4,131 @@ class Program04 extends StatelessWidget {
   const Program04({Key? key}) : super(key: key);
 
   @override
+  // layout 1
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: Text("Program_04")),
         body: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.red,
-                ),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.red,
-                ),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.red,
-                )
-              ],
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.red,
-                ),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.red,
-                ),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.red,
-                )
-              ],
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.red,
-                ),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.red,
-                ),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.red,
-                )
-              ],
-            )
+            Expanded(
+                child: Column(
+                  children: [
+                    Expanded(
+                        child: Container(
+                          color: Colors.red,
+                        )),
+                    Expanded(
+                        child: Container(
+                          color: Colors.brown,
+                        )),
+                    Expanded(
+                        child: Container(
+                          color: Colors.black,
+                        ))
+                  ],
+                )),
+            Expanded(
+                child: Column(
+                  children: [
+                    Expanded(
+                        child: Container(
+                          color: Colors.green,
+                        )),
+                    Expanded(
+                        child: Container(
+                          color: Colors.cyan,
+                        )),
+                    Expanded(
+                        child: Container(
+                          color: Colors.pink,
+                        ))
+                  ],
+                )),
+            Expanded(
+                child: Column(
+                  children: [
+                    Expanded(
+                        child: Container(
+                          color: Colors.blue,
+                        )),
+                    Expanded(
+                        child: Container(
+                          color: Colors.purple,
+                        )),
+                    Expanded(
+                        child: Container(
+                          color: Colors.orange,
+                        ))
+                  ],
+                ))
           ],
         ));
   }
+
+  // // layout 2
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //       appBar: AppBar(title: Text("Program_04")),
+  //       body: Row(
+  //         children: [
+  //           Expanded(
+  //               child: Column(
+  //             children: [
+  //               Expanded(
+  //                   child: Container(
+  //                 color: Colors.red,
+  //               )),
+  //               Expanded(
+  //                   child: Container(
+  //                 color: Colors.brown,
+  //               )),
+  //               Expanded(
+  //                   child: Container(
+  //                 color: Colors.black,
+  //               ))
+  //             ],
+  //           )),
+  //           Expanded(
+  //               child: Column(
+  //             children: [
+  //               Expanded(
+  //                   flex: 6,
+  //                   child: Container(
+  //                     color: Colors.green,
+  //                   )),
+  //               Expanded(
+  //                   flex: 4,
+  //                   child: Container(
+  //                     color: Colors.cyan,
+  //                   )),
+  //               Expanded(
+  //                   flex: 2,
+  //                   child: Container(
+  //                     color: Colors.pink,
+  //                   ))
+  //             ],
+  //           )),
+  //           Expanded(
+  //               child: Column(
+  //             children: [
+  //               Expanded(
+  //                   flex: 2,
+  //                   child: Container(
+  //                     color: Colors.blue,
+  //                   )),
+  //               Expanded(
+  //                   flex: 6,
+  //                   child: Container(
+  //                     color: Colors.purple,
+  //                   )),
+  //               Expanded(
+  //                   flex: 4,
+  //                   child: Container(
+  //                     color: Colors.orange,
+  //                   ))
+  //             ],
+  //           ))
+  //         ],
+  //       ));
+  // }
 }
