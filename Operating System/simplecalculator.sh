b9cc17e623b829a5dd33b88fb0d749d73278658c
Devanshu Shah <93487920+devanshushah294@users.noreply.github.com>
2023-01-03 01:06:19 +0530

    Update simplecalculator.sh

--- Operating System/simplecalculator.sh
+++ Operating System/simplecalculator.sh
@@ -1,30 +1,31 @@
-echo "Enter 2 numbers to operate on them:"
-read n1
-read n2
-echo "Enter the number of the function you want to perform:"
-echo "1. Addition"
-echo "2. Subtraction"
-echo "3. Multiplication"
-echo "4. Division"
-read f
+echo "Enter numbers 1 : ";
+read n1;
+echo "Enter numbers 2 : ";
+read n2;
+echo "Enter the number of the function you want to perform:";
+echo "1 for Addition";
+echo "2 for Subtraction";
+echo "3 for Multiplication";
+echo "4 for Division";
+read f;
 case $f in
 1)
-  ans=`expr $n1 + $n2`
-  echo "$ans"
+  ans=`expr $n1 + $n2`;
+  echo "answer is $ans";
   ;;
 2)
-  ans=`expr $n1 - $n2`
-  echo "$ans"
+  ans=`expr $n1 - $n2`;
+  echo "answer is $ans";
   ;;
 3)
-  ans=`expr $n1 \* $n2`
-  echo "$ans"
+  ans=`expr $n1 \* $n2`;
+  echo "answer is $ans";
   ;;
 4)
-  ans=`expr $n1 / $n2`
-  echo "$ans"
+  ans=`expr $n1 / $n2`;
+  echo "answer is $ans";
   ;;
 *)
-  echo "INVALID REQUEST"
+  echo "INVALID REQUEST";
   ;;
 esac
