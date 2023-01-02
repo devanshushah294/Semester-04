7c302339077a332735fa37785ecaff8d9585ab3a
Devanshu Shah <93487920+devanshushah294@users.noreply.github.com>
2023-01-03 01:06:16 +0530

    Update prime.sh

--- Operating System/prime.sh
+++ Operating System/prime.sh
@@ -1,17 +1,17 @@
-echo "Enter a number to check whether it is prime or not:"
-read n
-count=0
+echo "Enter a number to check whether it is prime or not:";
+read n;
+count=0;
 for((i=2;i<$n;i++))
 do
-  a=`expr $n % $i`
+  a=`expr $n % $i`;
   if [ $a -eq 0 ]
   then
-    count=`expr $count + 1`
+    count=`expr $count + 1`;
   fi
 done
 if [ $count == 0 ]
 then
-  echo "Prime number"
+  echo "Prime number";
 else
-  echo "Not a prime number"
+  echo "Not a prime number";
 fi
