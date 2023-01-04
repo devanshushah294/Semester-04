2514f58b3028232f600955cd622ffb9de53acd2b
Devanshu Shah <93487920+devanshushah294@users.noreply.github.com>
2023-01-04 17:18:31 +0530

    Update custom_column_widget.dart

--- Learning Flutter Projects/lab_07/lib/widgets/custom_column_widget.dart
+++ Learning Flutter Projects/lab_07/lib/widgets/custom_column_widget.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class CustomColumnWidget extends StatelessWidget {
   const CustomColumnWidget(
-      {Key? key, @required this.parts, List<dynamic>?this.color, List<dynamic>?this.flex})
+      {Key? key, @required this.parts, this.color, this.flex})
       : super(key: key);
   final flex;
   final parts;
@@ -18,8 +18,8 @@ class CustomColumnWidget extends StatelessWidget {
     List<Widget> widget = [];
     for (int i = 0; i < parts; i++) {
       widget.add(Expanded(
-          flex: flex[i] ?? 1,
-          child: Container(color: color[i] ?? Colors.transparent)));
+          flex: flex!=null?flex[i]:1,
+          child: Container(color: color!=null?color[i] : Colors.transparent)));
     }
     return widget;
   }
