c2b57993ce51a9fd660eb4f171c237753a3b47d7
Devanshu Shah <93487920+devanshushah294@users.noreply.github.com>
2023-01-25 01:39:27 +0530

    .

--- Learning Flutter Projects/my_flutter_app/lib/lab-08/check_page.dart
+++ Learning Flutter Projects/my_flutter_app/lib/lab-08/check_page.dart
@@ -31,10 +31,69 @@ class CheckPage extends StatefulWidget {
 }
 
 class _checkPageState extends State<CheckPage> {
+  TextEditingController abc = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
+      // body: Container(
+      //   child: Column(
+      //     children: [
+      //       Container(
+      //         child: TextFormField(
+      //           controller: abc,
+      //           decoration: InputDecoration(
+      //             label: Text("Enter Name"),
+      //             hintText: "hjhb",
+      //             helperText: "sfndgbk",
+      //             floatingLabelBehavior:FloatingLabelBehavior.auto,
+      //
+      //           ),
+      //         ),
+      //       )
+      //     ],
+      //   ),
+      // ),
+      body: Row(
+        children: [
+          Expanded(
+            child: Column(
+              children: devide(
+                3,
+                [Colors.red, Colors.green, Colors.blue],
+              ),
+            ),
+          ),
+          Expanded(
+            child: Column(
+              children: devide(
+                3,
+                [Colors.red, Colors.green, Colors.blue],
+              ),
+            ),
+          ),
+          Expanded(
+            child: Column(
+              children: devide(
+                3,
+                [Colors.red, Colors.green, Colors.blue],
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
+}
+
+List<Widget> devide(parts, color) {
+  List<Widget> widget = [];
+  for (int i = 0; i < parts; i++) {
+    widget.add(Expanded(
+        child: Container(
+      color: color[i] ?? Colors.white,
+    )));
+  }
+  return widget;
 }
